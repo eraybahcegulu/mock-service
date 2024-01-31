55064cdff55ba8ef44e247951fce2d679858d19c
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-01-31 15:59:19 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+async function deferRender() {
+	const { worker } = await import("./mocks/browser.js");
+	return worker.start();
+}
+
+deferRender().then(() => {
+	ReactDOM.createRoot(document.getElementById("root")).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+});
